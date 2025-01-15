e4ebaa67cc72db0ece34d7cf6cf14dbcc5c4d90b
Josh Walker <josh.walker1@outlook.com>
2025-01-15 12:51:28 +0000

    feat: Implemented post request handling and state management in Rocket

---
+++ src-tauri/src/server.rs
@@ -0,0 +1,57 @@
+use std::sync::Mutex;
+
+mod post;
+
+/* Placeholder ParserInfo structures for state management */
+#[allow(dead_code)] // Satisfy clippy - TODO: remove
+pub struct ParserInfo {
+	input: String,
+	tree: DebugTree,
+}
+
+pub struct DebugTree {}
+
+impl ParserInfo {
+	pub fn set_input(&mut self, _input: String) {
+		todo!()
+	}
+	
+	pub fn set_tree(&mut self, _tree: DebugTree) {
+		todo!()
+	}
+}
+
+
+/* Mount the Rocket server to the running instance of Tauri */
+pub fn mount() {
+    /* Placeholder parser info struct */
+    let parser_info: ParserInfo = ParserInfo {
+        input: String::from("This is a parser input"),
+        tree: DebugTree { }
+    };
+
+    /* Spawn the Rocket server */
+    tauri::async_runtime::spawn(async move {
+        rocket::build()
+            .mount("/", rocket::routes![hello, post::post]) /* Mount routes to the base path '/' */
+            .manage(Mutex::new(parser_info)) /* Manage the parser info as a mutex-protected state */
+            .launch() /* Launch the Rocket server */
+            .await
+            .expect("Rocket failed to initialise");
+    });
+}
+
+/* Placeholder GET request handler to print 'Hello world!' */
+#[rocket::get("/")]
+fn hello() -> String {
+    String::from("Hello world!")
+}
+
+
+
+#[cfg(test)]
+mod test {
+
+    // Server integration testing
+    
+}
---
+++ src-tauri/src/server/post.rs
@@ -0,0 +1,42 @@
+use std::sync::Mutex;
+use rocket::{post, serde::json::Json, http};
+
+use super::{DebugTree, ParserInfo};
+
+#[derive(serde::Deserialize)] /* Support Json deserialisation */
+struct ParsleyDebugTree { } 
+
+impl Into<DebugTree> for ParsleyDebugTree {
+    fn into(self) -> DebugTree {
+        todo!()
+    }
+}
+
+
+#[derive(serde::Deserialize)]
+struct Data {
+    input: String,
+    tree: ParsleyDebugTree,
+}
+
+
+#[allow(private_interfaces)] /* Satisfy clippy */
+/* Handle a post request containing parser data */
+#[post("/", format = "application/json", data = "<data>")] //TODO: check post route
+pub async fn post(data: Json<Data>, state: &rocket::State<Mutex<ParserInfo>>) -> http::Status {
+    /* Deserialise and unwrap json data */
+    let Data { input, tree } = data.into_inner(); 
+
+    /* Acquire the mutex to modify state */
+    match state.lock() {
+        /* If successful, update state and return ok http status */
+        Ok(mut state) => {
+            state.set_input(input);
+            state.set_tree(tree.into());
+            http::Status::Ok
+        },
+
+        /* If unsucessful, return error http status */
+        Err(_) => http::Status::Conflict
+    }
+}
