8a9281356fca8d0fbe5a5aa1e2309102278e3c3b
Josh <josh.ja.walker@outlook.com>
2025-01-16 20:31:17 +0000

    fix: use Rocket.toml config when launching server

--- src-tauri/src/server/launch.rs
+++ src-tauri/src/server/launch.rs
@@ -1,25 +1,33 @@
 use std::sync::Mutex;
 
-use rocket::Rocket;
+use rocket::{Rocket, Build, Ignite, Config};
+use rocket::figment::{Figment, providers::{Format, Toml}};
 
 use crate::{DebugTree, ParserInfo};
 
+/* Embed Rocket.toml as a string to allow post-compilation access */
+const ROCKET_CONFIG: &str = include_str!("Rocket.toml");
+
 
 /* Build the Rocket server */
-pub fn build() -> Rocket<rocket::Build> {
+pub fn build() -> Rocket<Build> {
     /* Placeholder parser info struct */
     let parser_info: ParserInfo = ParserInfo::new(
         String::from("This is a parser input"),
         DebugTree { }
     );
-    
-    rocket::build()
+
+    /* Override the default config with values from Rocket.toml */
+    let figment: Figment = Figment::from(Config::default())
+        .merge(Toml::string(ROCKET_CONFIG).nested());
+
+    rocket::custom(figment) /* Build the Rocket server with a custom config */
         .mount("/", super::request::routes()) /* Mount routes to the base path '/' */
         .manage(Mutex::new(parser_info)) /* Manage the parser info as a mutex-protected state */
 }
 
 /* Launch the Rocket server */
-pub async fn launch() -> Rocket<rocket::Ignite> {
+pub async fn launch() -> Rocket<Ignite> {
     build().launch().await
         .expect("Rocket failed to initialise")
 }
