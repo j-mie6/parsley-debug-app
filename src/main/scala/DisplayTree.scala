c23b809c98443427dc3ab32b221a2d7ebe64b390
Alejandro Perez Fadon <ap3222@ic.ac.uk>
2025-01-17 12:09:37 +0000

    style: Fixed element style in DisplayTree

--- src/main/scala/DisplayTree.scala
+++ src/main/scala/DisplayTree.scala
@@ -9,36 +9,41 @@ import com.raquo.laminar.api.L.*
   * @param children list of children of display tree's root node
   */
 class DisplayTree(val placeholderText: String, val children: List[DisplayTree]) {
+    
+    private val debugTreeElement: Element = {
+        div(
+            paddingLeft := "0",
+            listStyleType := "none",
+            border := "2px solid tomato",
+            padding := "2px",
+            currentNode,
+            ul(
+                listStyleType := "none",
+                children.map((t) => li(
+                    verticalAlign := "top",
+                    padding := "2px",
+                    t.element
+                ))
+            )
+        )
+    }
+
+    /**
+      * HTLM element to display DisplayTree
+      */
     def element: Element = {
         val currentNode: Element = p(placeholderText)
-        if (children.isEmpty) {
-            currentNode
-        } else {
-            div(
-                paddingLeft := "0",
-                listStyleType := "none",
-                border := "2px solid tomato",
-                padding := "2px",
-                currentNode,
-                ul(
-                    listStyleType := "none",
-                    children.map((t) => li(
-                        verticalAlign := "top",
-                        padding := "2px",
-                        t.element
-                    ))
-                )
-            )
-        }
+        if child.isEmpty then currentNode else debugTreeElement
     }
 }
 
 object DisplayTree {
-
     // Example of a DisplayTree to use in testing
-    final val SAMPLE_TREE: DisplayTree = DisplayTree("root!", List(DisplayTree("alejandro", List(DisplayTree("child"))), DisplayTree("adam"), DisplayTree("kevin")))
+    final val SAMPLE_TREE: DisplayTree = {
+        DisplayTree("root!", List(DisplayTree("alejandro", List(DisplayTree("child"))), DisplayTree("adam"), DisplayTree("kevin")))
+    }
     
     // Secondary constructors
     def apply(placeholderText: String, children: List[DisplayTree]): DisplayTree = new DisplayTree(placeholderText, children)
-    def apply(placeholderText: String): DisplayTree = DisplayTree(placeholderText, List())
+    def apply(placeholderText: String): DisplayTree = DisplayTree(placeholderText, List[DisplayTree]())
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,7 +4,6 @@ import com.raquo.laminar.api.L.*
 
 import Display.DisplayTree
 
-
   @main def hello(): Unit = renderOnDomContentLoaded(
     dom.document.getElementById("app"),
     appElement()
