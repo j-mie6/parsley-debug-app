9546496d8816fa08ecf1b88ffb51126e46e64a25
Josh <josh.ja.walker@outlook.com>
2025-01-18 13:32:25 +0000

    test(server.rs): added server stress integration test

--- src-tauri/src/server.rs
+++ src-tauri/src/server.rs
@@ -2,20 +2,41 @@ mod launch;
 mod data;
 mod request;
 
-pub use launch::launch as launch;
+pub use launch::launch;
 
 
 #[cfg(test)]
 mod test {
     
-    /* Server integration testing */
+    use rocket::{http, local::blocking};
+    use super::launch;
 
-    use rocket::local::blocking;
+    /* Server integration testing */
 
     /* Start a blocking, tracked client for rocket */
     pub fn tracked_client() -> blocking::Client {
-        blocking::Client::tracked(super::launch::build())
+        blocking::Client::tracked(launch::build())
             .expect("Rocket failed to initialise")
     }
+    
+    #[test]
+    fn server_handles_many_requests() {
+        let client: blocking::Client = tracked_client();
+        
+        /* Format GET request to index route '/' */
+        let get_request = client.get("/");
+        
+        /* Format POST requests to route '/remote' */
+        let post_request = client.post("/remote")
+            .header(http::ContentType::JSON)
+            .body(r#"{"input": "this is the parser input", "tree": "tree"}"#);
+
+
+        /* Repeatedly perform requests */
+        for _ in 0..100 {
+            assert_eq!(get_request.clone().dispatch().status(), http::Status::Ok);
+            assert_eq!(post_request.clone().dispatch().status(), http::Status::Ok);
+        }
+    }   
 
 }
