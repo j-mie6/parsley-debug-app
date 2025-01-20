7f74a950f5139450546ec2d65992b8cd2d620b27
rileyhorrix <riley@horrix.com>
2025-01-20 15:52:03 +0000

    refactor: change location of build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,13 @@ echo Starting DILL Build
 echo
 
 rm -f src-laminar/package.json &&\
+easf &&\
 cp -f package.json src-laminar/ && \
 sbt buildFrontend && \
 npm run tauri build
+
+if [ $? -ne 0 ]
+then
+    printf "\n\e[31mError In Starting Development Server\e[0m\n\n"
+    exit 1
+fi
--- scripts/run.sh
+++ scripts/run.sh
@@ -8,3 +8,9 @@ rm -f src-laminar/package.json &&\
 cp -f package.json src-laminar/ && \
 sbt buildFrontend && \
 npm run tauri dev
+
+if [ $? -ne 0 ]
+then
+    printf "\n\e[31mError In Starting Development Server\e[0m\n\n"
+    exit 1
+fi
