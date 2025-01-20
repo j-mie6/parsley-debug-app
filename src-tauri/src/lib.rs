1ddc8c02d53607db2ef6b97ce0ce7cb58d38a4ad
rileyHorrix <81515896+Riley-horrix@users.noreply.github.com>
2025-01-20 14:27:43 +0000

    refactor: Remove line in lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -18,7 +18,6 @@ pub fn run() {
       }
       Ok(())
     })
-    // .invoke_handler(tauri::generate_handler![greet])
     .invoke_handler(tauri::generate_handler![text])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
