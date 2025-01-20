29f4301c5dd5a88ab3443e6365b24ce5fb5f65a8
AdamW1087 <117484063+AdamW1087@users.noreply.github.com>
2025-01-20 18:55:17 +0000

    fix: Fixed bash syntax

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ echo Starting DILL Build
 echo
 
 # Linux Tauri v2 prerequisites
-if [ "$RUNNER_OS" == "Linux"]
+if [ "$RUNNER_OS" == "Linux" ]
 then
     apt update
     apt install libwebkit2gtk-4.1-dev \
