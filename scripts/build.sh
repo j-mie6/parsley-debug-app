2998e1b718fcb0e087c2dc0bf42a8c8b1ef6e1c5
Aniket Gupta <ag2922@ic.ac.uk>
2025-01-20 18:17:32 +0000

    feat: added Tauri v2 prerequisites for Linux in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,21 @@ npm install && \
 sbt buildFrontend && \
 npm run tauri build
 
+# Linux Tauri v2 prerequisites
+if [ $RUNNER_OS == "ubuntu-latest"]
+then
+    apt update
+    apt install libwebkit2gtk-4.1-dev \
+    build-essential \
+    curl \
+    wget \
+    file \
+    libxdo-dev \
+    libssl-dev \
+    libayatana-appindicator3-dev \
+    librsvg2-dev
+fi
+
 if [ $? -ne 0 ]
 then
     printf "\n\e[31mError In Starting Development Server\e[0m\n\n"
