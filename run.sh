8ba16bd59148eda147bd9708142cda703c8288f6
rileyhorrix <riley@horrix.com>
2025-01-20 15:43:14 +0000

    refactor: Handle build scripts and create proper build scripts

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!bin/bash
 
 echo
-echo Starting DILL Build
+echo Starting DILL Development Server
 echo
 
 rm -f src-laminar/package.json &&\
 cp -f package.json src-laminar/ && \
 sbt buildFrontend && \
-npm run tauri build
+npm run tauri dev
