1848311aa728c24a066c513063305058cf257d31
Josh <josh.ja.walker@outlook.com>
2025-01-21 14:42:13 +0000

    feat(clean.sh): Added echo prints

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,8 +1,16 @@
+#!bin/bash
+
+echo Removing Scala files...
 rm -rf .metals/
 rm -rf .bloop/
 rm -rf .bsp/
+
+echo Removing compiled App files...
 rm -rf static/
 rm -rf target/
 
+echo Removing Tauri and Laminar targets...
 rm -rf src-tauri/target/
 rm -rf src-laminar/target/
+
+echo Done
