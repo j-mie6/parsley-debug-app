de3eb063186e96089312551ac23bbf2138e615d0
Josh <josh.ja.walker@outlook.com>
2025-01-22 00:22:37 +0000

    style: Reindented Tauri with 4 spaces

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,38 +3,41 @@ mod parser_info;
 
 pub use parser_info::{ParserInfo, DebugTree};
 
+
 #[tauri::command]
 fn tree_text() -> String {
-  String::from("Tree is empty")
+    String::from("Tree is empty")
 }
+
 
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-          .level(log::LevelFilter::Info)
-          .build(),
-        )?;
-      }
-
-      /* Mount the Rocket server to the running instance of Tauri */
-      tauri::async_runtime::spawn(async {
-        server::launch().await.expect("Rocket failed to initialise")
-      });
-
-      Ok(())
-    })
-    .invoke_handler(tauri::generate_handler![tree_text])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                    .level(log::LevelFilter::Info)
+                    .build(),
+                )?;
+            }
+            
+            /* Mount the Rocket server to the running instance of Tauri */
+            tauri::async_runtime::spawn(async {
+                server::launch().await.expect("Rocket failed to initialise")
+            });
+            
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![tree_text])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
+
+
 
 #[cfg(test)]
 mod test {
-
-	/* Tauri integration tests */
-
+    
+    /* Tauri integration tests */
+    
 }
-
