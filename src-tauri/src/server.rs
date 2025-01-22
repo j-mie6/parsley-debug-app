436816bb424abacb197cf54cebc71dae1d2d01ff
Priyansh <priyanshchugh@outlook.com>
2025-01-22 14:15:54 +0000

    feat: Mocking POST requests in tests

--- src-tauri/src/server.rs
+++ src-tauri/src/server.rs
@@ -26,7 +26,7 @@ mod test {
 
     #[test]
     fn server_handles_one_request() {
-        let client: blocking::Client = tracked_client(rocket::routes![request::get_hello]);
+        let client: blocking::Client = tracked_client(request::test::mock_routes());
 
         /* Format GET request to index route '/' */
         let get_request = client.get("/");
@@ -35,23 +35,21 @@ mod test {
     
     #[test]
     fn server_handles_many_requests() {
-        let client: blocking::Client = tracked_client(rocket::routes![request::get_hello]);
+        let client: blocking::Client = tracked_client(request::test::mock_routes());
         
         /* Format GET request to index route '/' */
         let get_request = client.get("/");
-        
-        // TODO: Mock POST somehow
 
-        // /* Format POST requests to route '/api/remote' */
-        // let post_request = client.post("/api/remote")
-        //     .header(http::ContentType::JSON)
-        //     .body(r#"{"input": "this is the parser input", "tree": "tree"}"#);
+        /* Format POST requests to route '/api/remote' */
+        let post_request = client.post("/api/remote")
+            .header(http::ContentType::JSON)
+            .body("42");
         
         
         /* Repeatedly perform requests */
         for _ in 0..100 {
             assert_eq!(get_request.clone().dispatch().status(), http::Status::Ok);
-            // assert_eq!(post_request.clone().dispatch().status(), http::Status::Ok);
+            assert_eq!(post_request.clone().dispatch().status(), http::Status::Ok);
         }
     }   
     
--- src-tauri/src/server/request.rs
+++ src-tauri/src/server/request.rs
@@ -1,6 +1,6 @@
 use tauri::Manager;
 use std::sync::{Mutex, MutexGuard};
-use rocket::{http, post, serde::json::Json};
+use rocket::{http, serde::json::Json};
 
 use crate::{AppState, state::ParserInfo};
 use super::data::Data;
@@ -20,7 +20,7 @@ pub fn get_hello() -> String {
 
 
 /* Post request handler to accept parser info */
-#[post("/api/remote", format = "application/json", data = "<data>")] 
+#[rocket::post("/api/remote", format = "application/json", data = "<data>")] 
 pub fn post_tree(data: Json<Data>, state: &rocket::State<tauri::AppHandle>) -> http::Status {
     /* Deserialise json data and convert to ParserInfo */
     let parser_info: ParserInfo = data.into_inner().into();
@@ -39,30 +39,53 @@ pub fn post_tree(data: Json<Data>, state: &rocket::State<tauri::AppHandle>) -> h
 
 
 #[cfg(test)]
-mod test {
+pub mod test {
     
-    use crate::server::test::tracked_client;
+    use crate::server::{launch::MockHandle, test::tracked_client};
     use rocket::{http, local::blocking};
+    use super::*;
+
+    #[rocket::get("/")]
+    fn mock_get() -> String {
+        String::from("42")
+    }
+
+
+    #[rocket::post("/api/remote", format = "application/json", data = "<data>")] 
+    fn mock_post(data: Json<i32>, state: &rocket::State<MockHandle>) -> http::Status {
+        let _mock = state.inner();
+
+        let n = data.into_inner();
+        if n == 42 {
+            http::Status::Ok
+        } else {
+            http::Status::UnprocessableEntity
+        }
+    }
+
+    pub fn mock_routes() -> Vec<rocket::Route> {
+        rocket::routes![mock_get, mock_post]
+    }
     
     /* Request unit testing */
     
     #[test]
-    fn get_responds_hello_world() {
+    fn get_responds_42() {
         /* Launch rocket client via a blocking, tracked Client for debugging */
-        let client: blocking::Client = tracked_client(rocket::routes![super::get_hello]);
+        let client: blocking::Client = tracked_client(mock_routes());
         
         /* Perform GET request to index route '/' */
-        let response: blocking::LocalResponse = client.get(rocket::uri!(super::get_hello)).dispatch();
+        let response: blocking::LocalResponse = client.get(rocket::uri!(mock_get)).dispatch();
         
         /* Assert GET request was successful and payload was correct */
         assert_eq!(response.status(), http::Status::Ok);
-        assert_eq!(response.into_string().expect("Payload was not string"), "Hello world!");
+        assert_eq!(response.into_string().expect("Payload was not string"), "42");
     }
     
     #[test]
     fn unrouted_get_fails() {
         /* Launch rocket client via a blocking, tracked Client for debugging */
-        let client: blocking::Client = tracked_client(rocket::routes![super::get_hello]);
+        let client: blocking::Client = tracked_client(mock_routes());
         
         /* Perform GET request to non-existent route '/hello' */
         let response: blocking::LocalResponse = client.get("/hello").dispatch();
@@ -73,10 +96,10 @@ mod test {
     
     #[test]
     fn get_on_post_fails() {
-        let client: blocking::Client = tracked_client(vec![]);
+        let client: blocking::Client = tracked_client(mock_routes());
         
         /* Perform GET request to '/api/remote' */
-        let response: blocking::LocalResponse = client.get(rocket::uri!(super::post_tree)).dispatch();
+        let response: blocking::LocalResponse = client.get(rocket::uri!(mock_post)).dispatch();
         
         /* Assert that GET failed due to no found GET handlers */
         assert_eq!(response.status(), http::Status::NotFound);
@@ -84,13 +107,13 @@ mod test {
     
     
     #[test]
-    fn post_tree_succeeds() {
-        let client: blocking::Client = tracked_client(vec![]);
+    fn post_succeeds() {
+        let client: blocking::Client = tracked_client(mock_routes());
         
         /* Perform POST request to '/api/remote' */
-        let response: blocking::LocalResponse = client.post(rocket::uri!(super::post_tree))
+        let response: blocking::LocalResponse = client.post(rocket::uri!(mock_post))
             .header(http::ContentType::JSON)
-            .body(r#"{"input": "this is the parser input", "tree": "tree"}"#)
+            .body("42")
             .dispatch();
         
         /* Assert that POST succeeded */
@@ -99,10 +122,10 @@ mod test {
     
     #[test]
     fn empty_post_fails() {
-        let client: blocking::Client = tracked_client(vec![]);
+        let client: blocking::Client = tracked_client(mock_routes());
         
         /* Perform POST request to '/api/remote' */
-        let response: blocking::LocalResponse = client.post(rocket::uri!(super::post_tree))
+        let response: blocking::LocalResponse = client.post(rocket::uri!(mock_post))
             .header(http::ContentType::JSON)
             .body("{}")
             .dispatch();
@@ -113,10 +136,10 @@ mod test {
     
     #[test]
     fn bad_format_post_fails() {
-        let client: blocking::Client = tracked_client(vec![]);
+        let client: blocking::Client = tracked_client(mock_routes());
         
         /* Perform POST request to '/api/remote' */
-        let response: blocking::LocalResponse = client.post(rocket::uri!(super::post_tree))
+        let response: blocking::LocalResponse = client.post(rocket::uri!(mock_post))
             .header(http::ContentType::Text) /* Incompatible header type */
             .body("Hello world")
             .dispatch();
