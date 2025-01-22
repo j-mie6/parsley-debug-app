380eebb004b8bab7ce38ce4066c6fa92342f1fdd
Josh <josh.ja.walker@outlook.com>
2025-01-22 00:29:03 +0000

    style: Fixed styling on Scala imports

--- src-laminar/src/main/scala/Main.scala
+++ src-laminar/src/main/scala/Main.scala
@@ -1,17 +1,17 @@
 import org.scalajs.dom
-
 import scala.concurrent.ExecutionContext.Implicits.global
 
-import Display.DisplayTree
-
-import com.raquo.laminar.api.L._
+import com.raquo.laminar.api.L.*
 
 import lib.Tauri
+import Display.DisplayTree
+
 
 @main def app = renderOnDomContentLoaded(
     dom.document.getElementById("app"),
     appElement()
 )
+
 
 val textSignal: Var[String] = Var("Nothing")
 val tree_text: Div = div(text <-- textSignal)
