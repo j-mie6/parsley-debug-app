8b812299a228f807ea8e1b27bb2ce44bbb181b29
Josh <josh.ja.walker@outlook.com>
2025-01-22 00:22:07 +0000

    style: Reindented Scala with 4 spaces

--- src-laminar/src/main/scala/Main.scala
+++ src-laminar/src/main/scala/Main.scala
@@ -9,19 +9,19 @@ import com.raquo.laminar.api.L._
 import lib.Tauri
 
 @main def app = renderOnDomContentLoaded(
-  dom.document.getElementById("app"),
-  appElement()
+    dom.document.getElementById("app"),
+    appElement()
 )
 
 val textSignal: Var[String] = Var("Nothing")
 val tree_text: Div = div(text <-- textSignal)
 
 def appElement(): Div = div(
-  h1("Dill - Parsley Debugging"),
-  tree_text,
-  button(onClick --> { _ => {
-    for {
-      text <- Tauri.invoke[String]("tree_text")
-    } do textSignal.set(text)
-  }}, "Reload tree"),
+    h1("Dill - Parsley Debugging"),
+    tree_text,
+    button(onClick --> { _ => {
+        for {
+            text <- Tauri.invoke[String]("tree_text")
+        } do textSignal.set(text)
+    }}, "Reload tree"),
 )
