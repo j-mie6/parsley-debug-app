3948164ab0c6ba68dcdac8912869e4b15f28fbf8
aniket1101 <1.aniketgupta@gmail.com>
2025-01-23 21:38:15 +0000

    fix: Removed extraneous Object

--- src-laminar/src/main/scala/DebugTree.scala
+++ src-laminar/src/main/scala/DebugTree.scala
@@ -3,7 +3,3 @@ import upickle.default.{ReadWriter => RW, macroRW}
 
 case class DebugTree(name: String, internal: String, success: Boolean,
      input: String, number: Int, children: List[DebugTree]) derives RW
-
-object DebugTree {
-
-}
