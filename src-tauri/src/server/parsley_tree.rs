ffa1c74ec1d4822047a03f8ed71ebad3490cf32b
Adam <adamwatson1087@gmail.com>
2025-01-27 22:41:56 +0000

    refactor: Update tests to use simpler input

--- src-tauri/src/server/parsley_tree.rs
+++ src-tauri/src/server/parsley_tree.rs
@@ -101,54 +101,54 @@ pub mod test {
     #[test]
     fn nested_parsley_tree_deserialises() {
         let raw_tree: &str = r#"{
-            "input": "TestTest1Test1.1Test2Test2.1",
+            "input": "01234",
             "tree": {
-                "name": "Test",
-                "internal": "Test",
+                "name": "0",
+                "internal": "0",
                 "success": true,
                 "child_id": 0,
                 "from_offset": 0,
-                "to_offset": 3,
-                "input": "TestTest1Test1.1Test2Test2.1",
+                "to_offset": 0,
+                "input": "01234",
                 "children": [
                     {
-                        "name": "Test1",
-                        "internal": "Test1",
+                        "name": "1",
+                        "internal": "1",
                         "success": true,
                         "child_id": 1,
-                        "from_offset": 4,
-                        "to_offset": 8,
-                        "input": "TestTest1Test1.1Test2Test2.1",
+                        "from_offset": 1,
+                        "to_offset": 1,
+                        "input": "01234",
                         "children": [
                             {
-                                "name": "Test1.1",
-                                "internal": "Test1.1",
+                                "name": "2",
+                                "internal": "2",
                                 "success": true,
                                 "child_id": 2,
-                                "from_offset": 9,
-                                "to_offset": 15,
-                                "input": "TestTest1Test1.1Test2Test2.1",
+                                "from_offset": 2,
+                                "to_offset": 2,
+                                "input": "01234",
                                 "children": []
                             }
                         ]
                     },
                     {
-                        "name": "Test2",
-                        "internal": "Test2",
+                        "name": "3",
+                        "internal": "3",
                         "success": true,
                         "child_id": 3,
-                        "from_offset": 16,
-                        "to_offset": 20,
-                        "input": "TestTest1Test1.1Test2Test2.1",
+                        "from_offset": 3,
+                        "to_offset": 3,
+                        "input": "01234",
                         "children": [
                             {
-                                "name": "Test2.1",
-                                "internal": "Test2.1",
+                                "name": "4",
+                                "internal": "4",
                                 "success": true,
                                 "child_id": 4,
-                                "from_offset": 21,
-                                "to_offset": 27,
-                                "input": "TestTest1Test1.1Test2Test2.1",
+                                "from_offset": 4,
+                                "to_offset": 4,
+                                "input": "01234",
                                 "children": []
                             }
                         ]
@@ -160,58 +160,58 @@ pub mod test {
         let parsley_tree: ParsleyTree = serde_json::from_str(&raw_tree).expect("Could not deserialise nested ParsleyTree");
         
         /* Check that the root tree has been serialised correctly */
-        assert_eq!(parsley_tree.input, "TestTest1Test1.1Test2Test2.1");
-        assert_eq!(parsley_tree.tree.name, "Test");
-        assert_eq!(parsley_tree.tree.internal, "Test");
+        assert_eq!(parsley_tree.input, "01234");
+        assert_eq!(parsley_tree.tree.name, "0");
+        assert_eq!(parsley_tree.tree.internal, "0");
         assert_eq!(parsley_tree.tree.success, true);
         assert_eq!(parsley_tree.tree.child_id, 0);
         assert_eq!(parsley_tree.tree.from_offset, 0);
-        assert_eq!(parsley_tree.tree.to_offset, 3);
-        assert_eq!(parsley_tree.tree.input, "TestTest1Test1.1Test2Test2.1");
+        assert_eq!(parsley_tree.tree.to_offset, 0);
+        assert_eq!(parsley_tree.tree.input, "01234");
         assert_eq!(parsley_tree.tree.children.len(), 2);
 
         /* Check the first child */
         let child1 = &parsley_tree.tree.children[0];
-        assert_eq!(child1.name, "Test1");
-        assert_eq!(child1.internal, "Test1");
+        assert_eq!(child1.name, "1");
+        assert_eq!(child1.internal, "1");
         assert_eq!(child1.success, true);
         assert_eq!(child1.child_id, 1);
-        assert_eq!(child1.from_offset, 4);
-        assert_eq!(child1.to_offset, 8);
-        assert_eq!(child1.input, "TestTest1Test1.1Test2Test2.1");
+        assert_eq!(child1.from_offset, 1);
+        assert_eq!(child1.to_offset, 1);
+        assert_eq!(child1.input, "01234");
         assert_eq!(child1.children.len(), 1);
 
         /* Check the first grandchild of the first child */
         let child1_1 = &child1.children[0];
-        assert_eq!(child1_1.name, "Test1.1");
-        assert_eq!(child1_1.internal, "Test1.1");
+        assert_eq!(child1_1.name, "2");
+        assert_eq!(child1_1.internal, "2");
         assert_eq!(child1_1.success, true);
         assert_eq!(child1_1.child_id, 2);
-        assert_eq!(child1_1.from_offset, 9);
-        assert_eq!(child1_1.to_offset, 15);
-        assert_eq!(child1_1.input, "TestTest1Test1.1Test2Test2.1");
+        assert_eq!(child1_1.from_offset, 2);
+        assert_eq!(child1_1.to_offset, 2);
+        assert_eq!(child1_1.input, "01234");
         assert_eq!(child1_1.children.len(), 0);
 
         /* Check the second child */
         let child2 = &parsley_tree.tree.children[1];
-        assert_eq!(child2.name, "Test2");
-        assert_eq!(child2.internal, "Test2");
+        assert_eq!(child2.name, "3");
+        assert_eq!(child2.internal, "3");
         assert_eq!(child2.success, true);
         assert_eq!(child2.child_id, 3);
-        assert_eq!(child2.from_offset, 16);
-        assert_eq!(child2.to_offset, 20);
-        assert_eq!(child2.input, "TestTest1Test1.1Test2Test2.1");
+        assert_eq!(child2.from_offset, 3);
+        assert_eq!(child2.to_offset, 3);
+        assert_eq!(child2.input, "01234");
         assert_eq!(child2.children.len(), 1);
 
         /* Check the first grandchild of the second child */
         let child2_1 = &child2.children[0];
-        assert_eq!(child2_1.name, "Test2.1");
-        assert_eq!(child2_1.internal, "Test2.1");
+        assert_eq!(child2_1.name, "4");
+        assert_eq!(child2_1.internal, "4");
         assert_eq!(child2_1.success, true);
         assert_eq!(child2_1.child_id, 4);
-        assert_eq!(child2_1.from_offset, 21);
-        assert_eq!(child2_1.to_offset, 27);
-        assert_eq!(child2_1.input, "TestTest1Test1.1Test2Test2.1");
+        assert_eq!(child2_1.from_offset, 4);
+        assert_eq!(child2_1.to_offset, 4);
+        assert_eq!(child2_1.input, "01234");
         assert_eq!(child2_1.children.len(), 0);
     }
 
@@ -228,54 +228,54 @@ pub mod test {
     fn nested_parsley_debug_tree_converts_into_debug_tree() {
         /* Root tree to test */
         let parsley_debug_tree: ParsleyTree = ParsleyTree {
-            input: String::from("TestTest1Test1.1Test2Test2.1"),
+            input: String::from("01234"),
             tree: ParsleyNode {
-                name: String::from("Test"),
-                internal: String::from("Test"),
+                name: String::from("0"),
+                internal: String::from("0"),
                 success: true,
                 child_id: 0,
                 from_offset: 0,
-                to_offset: 3,
-                input: String::from("TestTest1Test1.1Test2Test2.1"),
+                to_offset: 0,
+                input: String::from("01234"),
                 children: vec![
                     ParsleyNode {
-                        name: String::from("Test1"),
-                        internal: String::from("Test1"),
+                        name: String::from("1"),
+                        internal: String::from("1"),
                         success: true,
                         child_id: 1,
-                        from_offset: 4,
-                        to_offset: 8,
-                        input: String::from("TestTest1Test1.1Test2Test2.1"),
+                        from_offset: 1,
+                        to_offset: 1,
+                        input: String::from("01234"),
                         children: vec![
                             ParsleyNode {
-                                name: String::from("Test1.1"),
-                                internal: String::from("Test1.1"),
+                                name: String::from("2"),
+                                internal: String::from("2"),
                                 success: true,
                                 child_id: 2,
-                                from_offset: 9,
-                                to_offset: 15,
-                                input: String::from("TestTest1Test1.1Test2Test2.1"),
+                                from_offset: 2,
+                                to_offset: 2,
+                                input: String::from("01234"),
                                 children: Vec::new()
                             }
                         ]
                     },
                     ParsleyNode {
-                        name: String::from("Test2"),
-                        internal: String::from("Test2"),
+                        name: String::from("3"),
+                        internal: String::from("3"),
                         success: true,
                         child_id: 3,
-                        from_offset: 16,
-                        to_offset: 20,
-                        input: String::from("TestTest1Test1.1Test2Test2.1"),
+                        from_offset: 3,
+                        to_offset: 3,
+                        input: String::from("01234"),
                         children: vec![
                             ParsleyNode {
-                                name: String::from("Test2.1"),
-                                internal: String::from("Test2.1"),
+                                name: String::from("4"),
+                                internal: String::from("4"),
                                 success: true,
                                 child_id: 4,
-                                from_offset: 21,
-                                to_offset: 27,
-                                input: String::from("TestTest1Test1.1Test2Test2.1"),
+                                from_offset: 4,
+                                to_offset: 4,
+                                input: String::from("01234"),
                                 children: Vec::new()
                             }
                         ]
@@ -285,44 +285,44 @@ pub mod test {
         };
 
         let debug_tree: DebugTree = DebugTree::new(
-            String::from("TestTest1Test1.1Test2Test2.1"), 
+            String::from("01234"), 
             DebugNode::new(
-                String::from("Test"), 
-                String::from("Test"), 
+                String::from("0"), 
+                String::from("0"), 
                 true, 
                 0, 
-                String::from("Test"), 
+                String::from("0"), 
                 vec![
                     DebugNode::new(
-                        String::from("Test1"), 
-                        String::from("Test1"), 
+                        String::from("1"), 
+                        String::from("1"), 
                         true, 
                         1, 
-                        String::from("Test1"), 
+                        String::from("1"), 
                         vec![
                             DebugNode::new(
-                                String::from("Test1.1"), 
-                                String::from("Test1.1"), 
+                                String::from("2"), 
+                                String::from("2"), 
                                 true, 
                                 2, 
-                                String::from("Test1.1"), 
+                                String::from("2"), 
                                 vec![]
                             )
                         ]
                     ),
                     DebugNode::new(
-                        String::from("Test2"), 
-                        String::from("Test2"), 
+                        String::from("3"), 
+                        String::from("3"), 
                         true, 
                         3, 
-                        String::from("Test2"), 
+                        String::from("3"), 
                         vec![
                             DebugNode::new(
-                                String::from("Test2.1"), 
-                                String::from("Test2.1"), 
+                                String::from("4"), 
+                                String::from("4"), 
                                 true, 
                                 4, 
-                                String::from("Test2.1"), 
+                                String::from("4"), 
                                 vec![]
                             )
                         ]
