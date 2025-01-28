2a1146ed712b9bf2d0cfba34d3a8ba869d5d3100
Josh Walker <josh.walker1@outlook.com>
2025-01-28 17:05:46 +0000

    fix(DebugTree): Updated Sample to use new structure

--- src-laminar/src/main/scala/DebugTree.scala
+++ src-laminar/src/main/scala/DebugTree.scala
@@ -13,14 +13,14 @@ object DebugTree {
     lazy final val Sample: DebugTree = DebugTree(
         "hi world!",
         DebugNode("~>", "~>", true, 0, "hi world!", List[DebugNode](
-            DebugNode("~>", "~>", true, 1, "hi world!", List[DebugNode](
-                DebugNode("'h'", "'h'", true, 1, "hi world!", List[DebugNode]()),
-                DebugNode("|", "|", true, 2, "hi world!", List[DebugNode](
-                    DebugNode("\"ello\"", "\"ello\"", false, 0, "hi world!", List[DebugNode]()),
-                    DebugNode("\"i\"", "\"i\"", true, 1, "hi world!", List[DebugNode]())
+            DebugNode("~>", "~>", true, 1, "hi", List[DebugNode](
+                DebugNode("'h'", "'h'", true, 1, "h", List[DebugNode]()),
+                DebugNode("|", "|", true, 2, "i", List[DebugNode](
+                    DebugNode("\"ello\"", "\"ello\"", false, 0, "", List[DebugNode]()),
+                    DebugNode("\"i\"", "\"i\"", true, 1, "i", List[DebugNode]())
                 ))
             )),
-            DebugNode("\" world!\"", "\" world!\"", true, 2, "hi world!", List[DebugNode]())
+            DebugNode("\" world!\"", "\" world!\"", true, 2, "world!", List[DebugNode]())
         ))
     )
 }
