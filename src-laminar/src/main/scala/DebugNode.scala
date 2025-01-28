561bcceaea78a891d827ac14133f3f4e9df7a691
Josh Walker <josh.walker1@outlook.com>
2025-01-28 17:22:16 +0000

    doc(DebugNode): Fix child_id incorrect param name

--- src-laminar/src/main/scala/DebugNode.scala
+++ src-laminar/src/main/scala/DebugNode.scala
@@ -7,7 +7,7 @@ import upickle.default.ReadWriter
   * @param name user defined name of the debug tree node
   * @param internal internal name of the debug tree node
   * @param success whether the input was successfully consumed
-  * @param child_id an unique identifier number for the tree node
+  * @param childId an unique identifier number for the tree node
   * @param input the input string the tree node has to parse
   * @param children list of children nodes
   */
