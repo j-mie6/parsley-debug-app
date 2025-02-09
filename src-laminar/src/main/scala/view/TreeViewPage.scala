6966f823c4ac6fe243d8a040fb4b7dd178cb00f4
Josh Walker <josh.walker1@outlook.com>
2025-02-09 02:06:31 +0000

    feat: Remove reload button

--- src-laminar/src/main/scala/view/TreeViewPage.scala
+++ src-laminar/src/main/scala/view/TreeViewPage.scala
@@ -17,23 +17,10 @@ import controller.Tauri
 object TreeViewPage extends DebugViewPage {
     private val displayTree: Var[HtmlElement] = Var(DebugTreeDisplay(DebugTree.Sample))
     
-    private lazy val reloadIcon: HtmlElement = i(className := "bi bi-arrow-clockwise", fontSize.px := 25, marginRight.px := 10)
-
-    private lazy val reloadButton: Element = button(
-        className := "tree-view-reload",
-        
-        reloadIcon,
-        "Reload tree",
-
-        onClick --> { _ => TreeController.reloadTree(displayTree)}
-    )
-
     def apply(): HtmlElement = {
         Tauri.listen[Unit]("tree-ready", {_ => TreeController.reloadTree(displayTree)})
         super.render(Some(div(
             className := "tree-view-page",
-
-            reloadButton,
             child <-- displayTree
         )))
     }
