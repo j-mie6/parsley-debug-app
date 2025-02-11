3158c67682808c5fbf93a94533df863be0fb00a1
aniket1101 <1.aniketgupta@gmail.com>
2025-02-11 22:50:04 +0000

    feat: Deleting tabs reactively without a reload button

--- frontend/src/main/scala/controller/TabController.scala
+++ frontend/src/main/scala/controller/TabController.scala
@@ -9,58 +9,79 @@ import upickle.default as up
 
 
 /**
-  * TabController object contains functions that modify the state of 
-  * saved/loaded trees
-  */
+* TabController object contains functions that modify the state of 
+* saved/loaded trees
+*/
 
 object TabController {
     /* Scrollable tab that holds all saved trees */
     private val scrollableTab: Var[HtmlElement] = Var(div())
-
+    
     /* List of file names (excluding path and ext) wrapped in Var */
     private var fileNames: Var[List[String]] = Var(Nil)
-
+    
     /**
-      * Gets the scrollable tab element
-      */
+    * Gets the scrollable tab element
+    */
     def getTab = scrollableTab
-
+    
+    /**
+      * 
+      *
+      * @param tab An individual tab comprising of title and delete button
+      */
     def setTab(tab: HtmlElement) = {
-      scrollableTab.set(tab)
+        scrollableTab.set(tab)
     }
-
+    
     /**
-      * Gets filenames of saved trees
-      */
+    * Gets filenames of saved trees
+    */
     def getFileNames: Var[List[String]] = {
-      fetchSavedTreeNames()
-      fileNames
+        fetchSavedTreeNames()
+        fileNames
     }
+    
+    
     /**
-      * 
-      *
-      * @param treeName User-defined name of tree to be saved
-      */
-    def saveTree(treeName: String): Unit = Tauri.invoke[String]("save_tree", Map("name" -> treeName))
-
-    /**
-      * 
-      *
-      * @param fileNames List of all trees saved by the user within a session
-      */
+    * 
+    *
+    * @param fileNames List of all trees saved by the user within a session
+    */
     def fetchSavedTreeNames(): Unit = {
         Tauri.invoke[String]("fetch_saved_tree_names").foreach { serializedNames =>
-            // Update fileNames with parsed names
+            /* Update fileNames with parsed names */
             fileNames.update(_ => up.read[List[String]](serializedNames))
         }
     }
-
+    
     /**
       * 
       *
-      * @param treeName User-defined name of the tree to be loaded
-      * @param displayTree Tree element to load and display in a given tree
+      * @param tabTitle
       */
+    def deleteTab(tabTitle: String): Unit = {
+        Tauri.invoke("delete_tree", Map("name" -> tabTitle))
+        fetchSavedTreeNames()
+        TreeController.reloadTree()
+    }
+    
+    /**
+    * 
+    *
+    * @param treeName User-defined name of tree to be saved
+    */
+    def saveTree(treeName: String): Unit = {
+        Tauri.invoke[String]("save_tree", Map("name" -> treeName))
+        fetchSavedTreeNames()
+    }
+    
+    /**
+    * 
+    *
+    * @param treeName User-defined name of the tree to be loaded
+    * @param displayTree Tree element to load and display in a given tree
+    */
     def loadSavedTree(treeName: String): Unit = {
         Tauri.invoke[String]("load_saved_tree", Map("name" -> treeName)).foreach { _ =>
             TreeController.reloadTree()
--- frontend/src/main/scala/view/components/ScrollableTabView.scala
+++ frontend/src/main/scala/view/components/ScrollableTabView.scala
@@ -5,6 +5,8 @@ import com.raquo.laminar.api.L.*
 import org.scalajs.dom
 
 import controller.TabController
+import controller.Tauri
+import controller.TreeController
 
 
 object ScrollableTabView {
@@ -23,43 +25,35 @@ object ScrollableTabView {
 
     /* Renders a tab button from a saved tree name */
     private def tabButton(tabTitle: String): HtmlElement = {
-        button(
+        div(
+            button(
             className := "tab-button",
-
+            
+            transition := "all 0.5s",
             tabTitle,
             button(
+                className := "close-tab-button",
+
                 borderColor := "transparent",
                 backgroundColor := "transparent",
+                transition := "all 0.5s",
                 i(className := "bi bi-x-circle"),
-                onClick --> {_ => ??? /* TAURI COMMAND FOR DELETING FILES */}
+
+                onClick --> {_ => TabController.deleteTab(tabTitle)}
             ),
 
             onClick --> {_ => TabController.loadSavedTree(tabTitle)}
+            ),
+            
         )
-    }
-   
-    
-    /* Clockwise arrow as a visual call to action for reloading save t*/
-    private lazy val reloadIcon: HtmlElement = i(className := "bi bi-arrow-clockwise",
-        fontSize.px := 25, marginRight.px := 10)
-
-    /* Button which updates fileNames with json file names. */
-    private lazy val reloadButton: Element = button(
-        className := "reload-saved-trees-button",
         
-        reloadIcon,
-
-        onClick --> { _ => TabController.fetchSavedTreeNames()}
-    )
+    }
     
     def apply(): HtmlElement = {
         div(
             className:= "scrollable-tab-bar",
-            reloadButton,
-                /**
-                 * Renders a list of buttons which will reload to whatever tree is 
-                 * pressed on
-                 */ 
+
+            /* Renders tabs */ 
             children <-- TabController.getFileNames.signal.map(names =>
                 names.map(name => tabButton(name)): Seq[HtmlElement]
             )
