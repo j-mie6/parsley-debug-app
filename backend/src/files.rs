e8f66c21444264d2dac5bd7ab670e5e7003f113a
Adam <adamwatson1087@gmail.com>
2025-02-17 17:13:55 +0000

    style: move map_err to newline

--- backend/src/files.rs
+++ backend/src/files.rs
@@ -7,7 +7,8 @@ pub const SAVED_TREE_DIR : &str = "./saved_trees/";
 
 /* Removes all saved_trees wiith the folder */
 pub fn delete_saved_trees() -> Result<(), FileError> {
-    fs::remove_dir_all(SAVED_TREE_DIR).map_err(|_| FileError::CleanDirError )?;
+    fs::remove_dir_all(SAVED_TREE_DIR)
+        .map_err(|_| FileError::DeleteDirFailed)?;
 
     Ok(())
 }
@@ -16,7 +17,8 @@ pub fn delete_saved_trees() -> Result<(), FileError> {
 pub fn make_saved_trees() -> Result<(), FileError> {
     /* If the folder for saved_trees does not exist, create it. */
     if !std::path::Path::new(SAVED_TREE_DIR).exists() {
-        std::fs::create_dir(SAVED_TREE_DIR).map_err(|_| FileError::MakeDirError)?;
+        std::fs::create_dir(SAVED_TREE_DIR)
+            .map_err(|_| FileError::CreateDirFailed)?;
     }
 
     Ok(())
