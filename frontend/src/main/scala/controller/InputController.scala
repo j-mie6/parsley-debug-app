4853b31111f55ddcf715161c5ee83519926129aa
aniket1101 <1.aniketgupta@gmail.com>
2025-02-17 17:34:39 +0000

    feat: Created InputController

---
+++ frontend/src/main/scala/controller/InputController.scala
@@ -0,0 +1,44 @@
+package controller
+
+import com.raquo.laminar.api.L.*
+
+object InputController {
+    /* The input element to be render by InputView */
+    private val inputElement: Var[HtmlElement] = Var(div())
+
+    /**
+     * Converts an input string to an HTML element
+     * 
+     * @param input The input string
+     * @return An HTML element displaying the input string
+     */
+    def toInputElement(input: String): HtmlElement = {
+        div (
+            h1(
+                className := "debug-tree-title",
+                p("Parser Input : ", margin.px := 0, fontSize.px := 15,
+                    fontStyle.italic, fontWeight.lighter),
+                input
+            ),
+        )
+    }
+
+    /**
+     * Gets the input string
+     * 
+     * @return An HTML element displaying the tree's input string
+     */
+    def getInput: HtmlElement = div(child <-- inputElement)
+    
+    /**
+     * Sets the input string
+     * 
+     * @param newInput The new input string
+     */
+
+    def setInput(input: String): Unit = {
+        inputElement.set(
+            toInputElement(input)
+        )
+    }
+}
