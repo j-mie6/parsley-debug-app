e2178f0cfabae4d6249e34ac3adc505301af4b0b
Adam <adamwatson1087@gmail.com>
2025-02-17 17:24:50 +0000

    style: Update path import

--- backend/src/files.rs
+++ backend/src/files.rs
@@ -16,7 +16,7 @@ pub fn delete_saved_trees() -> Result<(), FileError> {
 
 pub fn make_saved_trees() -> Result<(), FileError> {
     /* If the folder for saved_trees does not exist, create it. */
-    if !std::path::Path::new(SAVED_TREE_DIR).exists() {
+    if !Path::new(SAVED_TREE_DIR).exists() {
         fs::create_dir(SAVED_TREE_DIR)
             .map_err(|_| FileError::CreateDirFailed)?;
     }
