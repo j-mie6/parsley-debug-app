76c59a3c831cc4d52658be417716c6aeebb39115
Adam <adamwatson1087@gmail.com>
2025-02-17 16:47:38 +0000

    refacor: Made error enum for files

--- backend/src/files.rs
+++ backend/src/files.rs
@@ -6,26 +6,23 @@ pub const SAVED_TREE_DIR : &str = "./saved_trees/";
 
 
 /* Removes all saved_trees wiith the folder */
-pub fn clean_saved_trees() -> Result<(), CleanTreesError> {
-    fs::remove_dir_all(SAVED_TREE_DIR).map_err(|_| CleanTreesError::CleanTreesError)?;
+pub fn clean_saved_trees() -> Result<(), FileError> {
+    fs::remove_dir_all(SAVED_TREE_DIR).map_err(|_| FileError::CleanDirError )?;
     Ok(())
 }
 
-#[derive (Debug)]
-pub enum CleanTreesError {
-    CleanTreesError
-}
 
-pub fn make_saved_trees() -> Result<(), MakeSavedTreesError> {
+pub fn make_saved_trees() -> Result<(), FileError> {
     /* If the folder for saved_trees does not exist, create it. */
     if !std::path::Path::new(SAVED_TREE_DIR).exists() {
-        std::fs::create_dir(SAVED_TREE_DIR).map_err(|_| MakeSavedTreesError::MakeSavedTreesError)?;
+        std::fs::create_dir(SAVED_TREE_DIR).map_err(|_| FileError::MakeDirError)?;
     }
 
     Ok(())
 }
 
 #[derive (Debug)]
-pub enum MakeSavedTreesError {
-    MakeSavedTreesError
+pub enum FileError  {
+    CleanDirError,
+    MakeDirError,
 }
