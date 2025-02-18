8f1e02a285ca5e6ff634740e9088d4b25bf1b83e
Alejandro Perez Fadon <ap3222@ic.ac.uk>
2025-02-18 01:22:55 +0000

    feat: Started mapError function in ErrorHandler.scala

--- frontend/src/main/scala/view/ErrorHandler.scala
+++ frontend/src/main/scala/view/ErrorHandler.scala
@@ -24,11 +24,24 @@ object ErrorHandler {
     )
 
     /* Maps an error passed by the backend to a frontend DillException object */
-    /* I think the problem is errors are scala.js errors, so they are all 
-       js.JavaScriptException: https://www.scala-js.org/doc/interoperability/exceptions.html */
+    /* I think the problem is errors are scala.js errors, so they are all js.JavaScriptException: 
+       https://www.scala-js.org/doc/interoperability/exceptions.html
+       https://javadoc.io/doc/org.scala-js/scalajs-library_2.12/latest/scala/scalajs/js/JavaScriptException.html
+    */
     def mapError(error: Throwable): DillException = {
         error match {
-            case _ => new PracticeWarning("hey", "hi")
+            /* Backend errors */
+            case jsErr:js.JavaScriptException => jsErr match {
+                case err if err.getMessage().contains("TreeNotFound") => TreeNotFound
+
+                case _ => new UnknownError(s"Unknown backend error: ${jsErr.toString()}")
+            }
+
+            /* Frontend errors */
+            case err:MalformedJSON => MalformedJSON
+            
+            /* Unknown error if not from backend or frontend */
+            case _ => new UnknownError(s"Unknown error: ${error.toString()}")
         }
     }
 
