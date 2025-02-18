8afd4f903eddab41607ba44ba7a55b3a4f017455
Josh Walker <josh.walker1@outlook.com>
2025-02-18 15:06:13 +0000

    fix(Command): Remove generic from invoke

--- frontend/src/main/scala/controller/tauri/Command.scala
+++ frontend/src/main/scala/controller/tauri/Command.scala
@@ -33,7 +33,7 @@ sealed trait Command(val name: String) {
      * @param args Map of argument name to argument value. Argument names should be CamelCase.
      * @return EventStream holding the result of the command call
      */
-    protected[tauri] def invoke[T](args: Map[String, Any]): EventStream[this.Response] = 
+    protected[tauri] def invoke(args: Map[String, Any]): EventStream[this.Response] = 
         EventStream.fromJsPromise(
             tauriInvoke[String](this.name, StringDictionary(args.toSeq*)),
             emitOnce = true
