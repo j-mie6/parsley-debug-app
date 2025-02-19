50a6cb59d73c80fe587996941b63d39c4b0c8c0b
aniket1101 <1.aniketgupta@gmail.com>
2025-02-19 00:37:32 +0000

    pr: Added doc string for apply method in TreeView

--- frontend/src/main/scala/view/TreeView.scala
+++ frontend/src/main/scala/view/TreeView.scala
@@ -15,7 +15,13 @@ import controller.viewControllers.TreeViewController
   * Object containing rendering functions for the TreeView
   */
 object TreeView {
+
+    /**
+      * Converts a tree to an HTML element
+      *
+      * @return An HTML element displaying the tree
+      */
     def apply(): HtmlElement = div(
-            TreeViewController.getDisplayTree, /* Renders tree */
+        TreeViewController.getDisplayTree, /* Renders the tree */
     )
 }
