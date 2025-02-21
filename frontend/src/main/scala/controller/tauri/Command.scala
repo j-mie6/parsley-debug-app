084fa5e6c0f0555c0434c018f4418e62ab83f2c4
Priyansh <priyanshchugh@outlook.com>
2025-02-21 11:48:04 +0000

    feat: Frontend Command for breakpoint skipping

--- frontend/src/main/scala/controller/tauri/Command.scala
+++ frontend/src/main/scala/controller/tauri/Command.scala
@@ -11,6 +11,9 @@ enum Command(val name: String):
     case LoadSavedTree extends Command("load_saved_tree")
     case DeleteTree extends Command("delete_tree")
 
+    /* Breakpoint commands */
+    case SkipBreakpoints extends Command("skip_breakpoints")
+
 
 object Command {
     given commandToString: Conversion[Command, String] with
