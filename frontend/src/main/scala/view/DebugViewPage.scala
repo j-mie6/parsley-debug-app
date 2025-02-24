e915e0cbb218c6dc43c6534734f6649564de6c6c
rileyhorrix <riley@horrix.com>
2025-02-24 15:36:48 +0000

    feat: Moved icons into the view section & expanding

--- frontend/src/main/scala/view/DebugViewPage.scala
+++ frontend/src/main/scala/view/DebugViewPage.scala
@@ -2,6 +2,7 @@ package view
 
 import com.raquo.laminar.codecs.*
 import com.raquo.laminar.api.L.*
+import com.raquo.laminar.api.features.unitArrows
 
 import org.scalajs.dom
 
@@ -25,14 +26,14 @@ val gridTemplateColumns: StyleProp[String] = styleProp("grid-template-columns")
   */
 abstract class DebugViewPage extends Page {
     /* Bootstrap icons used on the page (https://icons.getbootstrap.com/) */
-    private lazy val treeIcon: Element = i(className := "bi bi-tree-fill", fontSize.px := 30)
-    private lazy val fileIcon: Element = i(className := "bi bi-file-earmark-text-fill", fontSize.px := 25)
-    private lazy val gitIcon: Element = i(className := "bi bi-github", fontSize.px := 40)
-    private lazy val sunIcon: Element = i(className := "bi bi-brightness-high-fill", fontSize.px := 33)
-    private lazy val moonIcon: Element = i(className := "bi bi-moon-fill", fontSize.px := 33)
+    private lazy val treeIcon: HtmlElement = i(className := "bi bi-tree-fill", fontSize.px := 30)
+    private lazy val fileIcon: HtmlElement = i(className := "bi bi-file-earmark-text-fill", fontSize.px := 25)
+    private lazy val gitIcon: HtmlElement = i(className := "bi bi-github", fontSize.px := 40)
+    private lazy val sunIcon: HtmlElement = i(className := "bi bi-brightness-high-fill", fontSize.px := 33)
+    private lazy val moonIcon: HtmlElement = i(className := "bi bi-moon-fill", fontSize.px := 33)
 
     /* Left section of the page header, containing tree and source view tabs */
-    private lazy val headerLeft: Element = div(
+    private lazy val headerLeft: HtmlElement = div(
         className := "debug-view-header-left",
         div(
             className := "debug-view-header-left-container",
@@ -55,7 +56,7 @@ abstract class DebugViewPage extends Page {
     )
 
     /* Title element */
-    private lazy val title: Element = div(
+    private lazy val title: HtmlElement = div(
         className := "debug-view-header-title",
         h1("Dill", fontSize.px := 40, margin.px := 0)
     )
@@ -63,18 +64,11 @@ abstract class DebugViewPage extends Page {
     /* Random number generator */
     private val rand = new scala.util.Random
 
-    /* Visual call to action for user to save the current tree */
-     private lazy val saveIcon: HtmlElement = i(
-        className := "bi bi-floppy-fill",
-        fontSize.px := 25, marginRight.px := 10
-    )
-
     /* Adds ability to save and store current tree. */
-    private lazy val saveButton: Element = button(
-        className := "save-button",
-
-        saveIcon, /* Floppy disk icon */
-
+    private lazy val saveButton: HtmlElement = button(
+        className := "debug-view-button-save debug-view-button",
+        i(className := "bi bi-floppy-fill"),
+        
         onClick --> { _ => {
             val treeName: String = (rand.nextInt).toString()
             TabViewController.saveTree(treeName)
@@ -82,8 +76,74 @@ abstract class DebugViewPage extends Page {
         }}
     )
 
+    /* Overview information tab. */
+    private lazy val infoButton: HtmlElement = button(
+        // TODO: When event streams and errors are fixed, make this emit actual information.
+        className := "debug-view-button debug-view-button-info",
+        i(className := "bi bi-info-circle-fill"),
+    )
+
+    /* Opener for the settings tab. */
+    private lazy val settingsTabButton: HtmlElement = button(
+        className := "debug-view-button debug-view-button-settings",
+        i(className := "bi bi-gear-wide-connected")
+    )
+
+    private val viewSelectorsExpanded : Var[Boolean] = Var(false)
+
+    private lazy val treeViewTabButton: HtmlElement = button(
+        className := "debug-view-select-button debug-view-tree-button",
+        cls("selected") <-- MainViewController.isTreeView(true),
+
+        i(className := "bi bi-tree-fill"),
+
+        div(
+            className := "debug-view-expand-button debug-view-expand-tree",
+            cls("expanded") <-- viewSelectorsExpanded.signal,
+            p("Tree View", marginLeft.px := 5),
+        ),
+
+        onClick --> { _ => 
+            MainViewController.setIsTreeView(true) 
+        },
+    )
+
+    private lazy val sourceViewTabButton: HtmlElement = button(
+        className := "debug-view-select-button debug-view-source-button",
+        i(className := "bi bi-file-earmark-text-fill"),
+        
+        cls("selected") <-- MainViewController.isTreeView(false),
+
+        div(
+            className := "debug-view-expand-button debug-view-expand-source",
+            cls("expanded") <-- viewSelectorsExpanded.signal,
+            p("Source View", marginLeft.px := 5)
+        ),
+
+        onClick --> { _ => 
+            MainViewController.setIsTreeView(false) 
+        },
+    )
+
+    private lazy val codeViewTabButton: HtmlElement = button(
+        className := "debug-view-select-button debug-view-code-button",
+        i(className := "bi bi-file-earmark-code-fill"),
+        
+        // cls("selected") <-- MainViewController.isTreeView(true),
+
+        div(
+            className := "debug-view-expand-button debug-view-expand-code",
+            cls("expanded") <-- viewSelectorsExpanded.signal,
+            p("Code View", marginLeft.px := 5),
+        ),
+
+        onClick --> { _ => 
+            // MainViewController.setIsTreeView(true) 
+        },
+    )
+
     /* Button used to toggle the theme */
-    private lazy val themeButton: Element = div(
+    private lazy val themeButton: HtmlElement = div(
         child <-- AppStateController.isLightMode.signal
             .map((project: Boolean) => if project then moonIcon else sunIcon),
         cursor.pointer,
@@ -93,29 +153,57 @@ abstract class DebugViewPage extends Page {
     )
 
     /* Button that links to the 'parsley-debug-app' Github repo */
-    private lazy val githubButton: Element = a(
+    private lazy val githubButton: HtmlElement = a(
         href := "https://github.com/j-mie6/parsley-debug-app", target := "_blank", gitIcon
     )
 
     /* Right section of the page header, containing various buttons */
-    private lazy val headerRight: Element = div(
+    private lazy val headerRight: HtmlElement = div(
         className := "debug-view-header-right",
         div(
             className := "debug-view-header-right-buttons",
-            saveButton,
+            // saveButton,
             themeButton,
             githubButton
         )
     )
 
     /* The page header */
-    private lazy val headerView: Element = headerTag(
+    private lazy val headerView: HtmlElement = headerTag(
         className := "debug-view-header",
 
-        headerLeft,
+        // headerLeft,
+        div(),
         title,
         headerRight,
-    )        
+    )
+
+    /* Button bar internal to the view. */
+    private lazy val buttonBar: HtmlElement = div(
+        className := "debug-view-button-bar",
+        /* Button bar left. */
+        div(
+            display.flex,
+            alignItems.center,
+
+            onMouseEnter --> viewSelectorsExpanded.set(true),
+            onMouseLeave --> viewSelectorsExpanded.set(false),
+
+
+            settingsTabButton,
+            treeViewTabButton,
+            sourceViewTabButton,
+            codeViewTabButton,
+        ),
+        /* Button bar right. */
+        div(
+            display.flex,
+            alignItems.center,
+
+            saveButton,
+            infoButton,
+        )
+    )   
 
     /**
       * Render the DebugViewPage header and a child element. This allows different views to 
@@ -133,6 +221,7 @@ abstract class DebugViewPage extends Page {
             TabView(),
             div(
                 className := "tree-view-page",
+                buttonBar,
                 child.getOrElse(div())
             )
         )))
