480664955adc5b07519095ed36fdd44ef1920b1e
aniket1101 <1.aniketgupta@gmail.com>
2025-02-24 14:43:05 +0000

    pr: Changed defs tht return Signals or Vars to vals

--- frontend/src/main/scala/controller/viewControllers/TabViewController.scala
+++ frontend/src/main/scala/controller/viewControllers/TabViewController.scala
@@ -22,41 +22,41 @@ object TabViewController {
     private val fileNames: Var[List[String]] = Var(Nil)
 
     /** Get file name associated with tab at index */
-    def getFileName(index: Int): Signal[String] = fileNames.signal.map(_(index))
+    val getFileName(index: Int): Signal[String] = fileNames.signal.map(_(index))
 
     /** Get list of file names */
-    def getFileNames: Signal[List[String]] = fileNames.signal
+    val getFileNames: Signal[List[String]] = fileNames.signal
 
     /** Set file names */
-    def setFileNames: Observer[List[String]] = fileNames.writer
+    val setFileNames: Observer[List[String]] = fileNames.writer
     
     /** Add name to file names */
-    def addFileName: Observer[String] = fileNames.updater((names, name) => names :+ name)
+    val addFileName: Observer[String] = fileNames.updater((names, name) => names :+ name)
     
     /** Fetches all tree names saved by the user from the backend */
     def loadFileNames: EventStream[List[String]] = Tauri.invoke(Command.FetchSavedTreeNames, ()).collectRight
 
     /** Returns true if there are no saved trees */
-    def noSavedTrees: Signal[Boolean] = getFileNames.signal.map(_.isEmpty)
+    val noSavedTrees: Signal[Boolean] = getFileNames.signal.map(_.isEmpty)
 
     
     /* Index of tab that is currented selected */
     private lazy val selectedTab: Var[Int] = Var(0)
 
     /** Set current selected tab index */
-    def setSelectedTab: Observer[Int] = selectedTab.writer
+    val setSelectedTab: Observer[Int] = selectedTab.writer
 
     /** Get index of tab with associated name */
-    def getFileNameIndex(name: String): Signal[Int] = getFileNames.map(_.indexOf(name))
+    val getFileNameIndex(name: String): Signal[Int] = getFileNames.map(_.indexOf(name))
             
     /** Get selected tab index */
-    def getSelectedTab: Signal[Int] = selectedTab.signal
+    val getSelectedTab: Signal[Int] = selectedTab.signal
     
     /** Get name of tree associated with selected tab */
-    def getSelectedFileName: Signal[String] = getSelectedTab.flatMapSwitch(getFileName)
+    val getSelectedFileName: Signal[String] = getSelectedTab.flatMapSwitch(getFileName)
 
     /** Checks if tab is currently selected */
-    def tabSelected(index: Int): Signal[Boolean] = getSelectedTab.map(_ == index)
+    val tabSelected(index: Int): Signal[Boolean] = getSelectedTab.map(_ == index)
 
 
     /** Saves current tree to the backend with given name, returning assigned tab index  */
