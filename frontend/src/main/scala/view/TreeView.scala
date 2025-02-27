0ca679358c203628b395d828055ce73dcba84270
Alejandro Perez Fadon <ap3222@cloud-vm-42-36.doc.ic.ac.uk>
2025-02-27 13:28:07 +0000

    feat: Implemented getting current tree and calling download tree on it

--- frontend/src/main/scala/view/TreeView.scala
+++ frontend/src/main/scala/view/TreeView.scala
@@ -9,18 +9,26 @@ import com.raquo.laminar.api.features.unitArrows
 
 import model.DebugTree
 import controller.viewControllers.TreeViewController
+import controller.viewControllers.TabViewController
 
 /* Object containing rendering functions for the TreeView */
 object TreeView {
 
+    /* Gets name of current tree and passes it to downloadTree */
+    def downloadCurrentTree: Unit = {
+        TabViewController.getSelectedFileName.changes.recoverToTry.collectSuccess
+            .flatMapMerge(TreeViewController.downloadTree) --> Observer.empty
+    }
+
+    /* Export tree button */
     val downloadButton: HtmlElement = button(
         className := "tree-view-download-button",
 
         /* Save button icon */
         i(className := "bi bi-download", fontSize.px := 35),
 
-        /* Deletes the respective tab */
-        onClick --> println("Stop tapping me!!!!"),
+        /* Exports current tree */
+        onClick --> downloadCurrentTree,
     )
 
     /* Render tree as HtmlElement */
