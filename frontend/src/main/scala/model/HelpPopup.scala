957333a6a83c0a272ff9ac01fbc6c43fce953518
aniket1101 <1.aniketgupta@gmail.com>
2025-03-03 01:31:45 +0000

    docs: Added doc string for HelpSection

--- frontend/src/main/scala/model/HelpPopup.scala
+++ frontend/src/main/scala/model/HelpPopup.scala
@@ -4,7 +4,14 @@ import com.raquo.laminar.api.L.*
 
 import controller.viewControllers.HelpViewController
 
-
+/**
+  * HelpSection is the generic representation of each section in the help popup for Dill
+  * 
+  * @param title Title of the section
+  * @param infoText Supplementary text that further describes an action
+  * @visualSource The relative path to an image that shows the action
+  * @index The index of this section amongst all help sections
+  */
 sealed trait HelpSection {
     def title: String
     def infoText: String
@@ -29,14 +36,14 @@ case object HelpAttachParser extends HelpSection {
 case object HelpNavigatingUI extends HelpSection {
     override def title: String = "2. Navigate the debug tree"
     override def infoText: String = "Use the tab system to switch between different trees and configurations."
-    override def visualSource: String = "assets/help/navigating_ui.png"
+    override def visualSource: String = "assets/help/navigating_ui.gif"
     override def index: Int = 2
 }
 
 case object HelpUnderstandingTrees extends HelpSection {
     override def title: String = "3. Understanding Parse Trees"
     override def infoText: String = "Parse trees represent the structure of your parsed input."
-    override def visualSource: String = "assets/help/understanding_trees.png"
+    override def visualSource: String = "assets/help/understanding_trees.gif"
     override def index: Int = 3
 }
 
