498dcea51169701cb4acd8bf91c8b95ecbc3116a
aniket1101 <1.aniketgupta@gmail.com>
2025-03-03 01:26:34 +0000

    feat: Implemented first GIF for help view

--- frontend/src/main/scala/model/HelpPopup.scala
+++ frontend/src/main/scala/model/HelpPopup.scala
@@ -15,26 +15,26 @@ sealed trait HelpSection {
 case object BaseHelpSection extends HelpSection {
     override def title: String = "How to Dill"
     override def infoText: String = "Getting started"
-    override def visualSource: String = "assets/help/attach_parser.png"
+    override def visualSource: String = "assets/help/basicImage.png"
     override def index: Int = 0
 }
 
 case object HelpAttachParser extends HelpSection {
-    override def title: String = "How to Attach a Parser"
-    override def infoText: String = "Get started by attaching a DillRemoteView to your Parsley parser."
-    override def visualSource: String = "assets/help/attach_parser.png"
+    override def title: String = "1. Attach a Parser"
+    override def infoText: String = "Don't forget to use the @experimental and @parsley.debuggable tags"
+    override def visualSource: String = "frontend/src/assets/help/attachParser.gif"
     override def index: Int = 1
 }
 
 case object HelpNavigatingUI extends HelpSection {
-    override def title: String = "Navigating the UI"
+    override def title: String = "2. Navigate the debug tree"
     override def infoText: String = "Use the tab system to switch between different trees and configurations."
     override def visualSource: String = "assets/help/navigating_ui.png"
     override def index: Int = 2
 }
 
 case object HelpUnderstandingTrees extends HelpSection {
-    override def title: String = "Understanding Parse Trees"
+    override def title: String = "3. Understanding Parse Trees"
     override def infoText: String = "Parse trees represent the structure of your parsed input."
     override def visualSource: String = "assets/help/understanding_trees.png"
     override def index: Int = 3
