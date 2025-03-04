690490efb8d83a2908204a1b7e14b92cceebf101
aniket1101 <1.aniketgupta@gmail.com>
2025-03-04 11:28:30 +0000

    feat: Implemented settings as a state

--- frontend/src/main/scala/model/Settings.scala
+++ frontend/src/main/scala/model/Settings.scala
@@ -3,34 +3,56 @@ package model.settings
 import com.raquo.laminar.api.L.*
 
 import controller.viewControllers.SettingsViewController
-
-type SettingVar = Var[Int] | Var[Boolean]
-
-sealed trait UserSetting {
+sealed trait UserSetting[T] {
     def settingName: String
     def infoText: String
-    def settingVar: SettingVar
+    def value: Var[T]
+
+    def setValue(newValue: T): Unit = {
+        value.set(newValue)
+    }
 }
 
-case object NumSkipIterativeChildren extends UserSetting {
+case class Settings(numSkipIterativeChildren: Int, numSkipBreakpoints: Int, colorBlindMode: Boolean)
+
+object Settings {
+    def empty = Settings(numSkipIterativeChildren = 5, numSkipBreakpoints = 1, colorBlindMode = false)
+
+    val settings = Var[Settings](Settings.empty)
+
+    val numSkipIterativeChildren =
+        settings.zoomLazy(_.numSkipIterativeChildren)((settings, numSkipIterativeChildren) => 
+            settings.copy(numSkipIterativeChildren = numSkipIterativeChildren)
+    )
+    val numSkipBreakpoints =
+        settings.zoomLazy(_.numSkipBreakpoints)((settings, numSkipBreakpoints) => 
+            settings.copy(numSkipBreakpoints = numSkipBreakpoints)
+    )
+    val colorBlindMode =
+        settings.zoomLazy(_.colorBlindMode)((settings, colorBlindMode) => 
+            settings.copy(colorBlindMode = colorBlindMode)
+    )
+}
+
+case object NumSkipIterativeChildren extends UserSetting[Int] {
     override def settingName: String = "Iterative children to skip"
     override def infoText: String = "The number of iterative children to skip with double arrows"
-    override def settingVar: SettingVar = SettingsViewController.getNumSkipIterativeChildren
+    override def value: Var[Int] = SettingsViewController.getNumSkipIterativeChildren
 }
 
-case object NumSkipBreakpoints extends UserSetting {
+case object NumSkipBreakpoints extends UserSetting[Int] {
     override def settingName: String = "Breakpoints to skip"
     override def infoText: String = "The number of breakpoints to skip forwards when in debug mode"
-    override def settingVar: SettingVar = SettingsViewController.getNumSkipBreakpoints
+    override def value: Var[Int] = SettingsViewController.getNumSkipBreakpoints
 }
 
-case object ColorBlindMode extends UserSetting {
+case object ColorBlindMode extends UserSetting[Boolean] {
     override def settingName: String = "Colour Blind Mode"
     override def infoText: String = "Set Dill's theme to a more accessible viewing experience"
-    override def settingVar: SettingVar = SettingsViewController.getColorBlindMode
+    override def value: Var[Boolean] = SettingsViewController.getColorBlindMode
 }
 
-val allUserSettings: List[UserSetting] = List(
+val allUserSettings = List(
     NumSkipIterativeChildren,
     NumSkipBreakpoints,
     ColorBlindMode
