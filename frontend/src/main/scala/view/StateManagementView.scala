ad70e99b7bce258690f61a63ffd746808ab2b3f6
aniket1101 <1.aniketgupta@gmail.com>
2025-03-05 03:32:39 +0000

    feat: Basic implementation of state management panel

---
+++ frontend/src/main/scala/view/StateManagementView.scala
@@ -0,0 +1,33 @@
+package view
+
+import com.raquo.laminar.api.L.*
+
+import model.toast.StateApplied
+import controller.ToastController
+import controller.viewControllers.StateManagementViewController
+
+object StateManagementView {
+    def apply(): HtmlElement = {
+        div(
+            className := "state-sidepanel-container",
+            cls("open") <-- StateManagementViewController.isStateOpen,
+
+            div(
+                className := ("state-sidepanel"),
+                
+                div(cls("state-header"),
+                    h2("State Management")
+                ),
+
+                div(
+                    cls("state-footer"),
+                    button(className := "apply-state-button", "Apply", onClick --> (_ => 
+                        
+
+                        ToastController.setToast(StateApplied)
+                    )),
+                ),
+            )
+        )
+    }
+}
