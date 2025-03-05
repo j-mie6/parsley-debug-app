dcab3d40b7a6d969d82fdc302193bcce08606293
aniket1101 <1.aniketgupta@gmail.com>
2025-03-05 00:48:08 +0000

    feat: Implemented all User Settings

--- frontend/src/main/scala/model/UserSettings.scala
+++ frontend/src/main/scala/model/UserSettings.scala
@@ -32,32 +32,10 @@ sealed trait UserSetting {
     def settingName: String
     def infoText: String
     def style: String
+    def default: UserSettingType
 
     /* Used as a temporary store of the setting prior to applying it platform-wide */
     def value: Var[UserSettingType]
-}
-
-case object NumSkipIterativeChildren extends UserSetting {
-    override def settingName: String = "Iterative children to skip"
-    override def infoText: String = "The number of iterative children to skip with double arrows"
-    override def style: String = "number"
-    override def value: Var[UserSettingType] = SettingsViewController.getNumSkipIterativeChildren.asInstanceOf[Var[UserSettingType]]
-}
-
-case object NumSkipBreakpoints extends UserSetting {
-    override def settingName: String = "Breakpoints to skip"
-    override def infoText: String = "The number of breakpoints to skip forwards when in debug mode"
-    override def style: String = "number"
-
-    override def value: Var[UserSettingType] = SettingsViewController.getNumSkipBreakpoints.asInstanceOf[Var[UserSettingType]]
-}
-
-case object ColorBlindMode extends UserSetting {
-    override def settingName: String = "Colour Blind Mode"
-    override def infoText: String = "Set Dill's theme to a more accessible viewing experience"
-    override def style: String = "boolean"
-
-    override def value: Var[UserSettingType] = SettingsViewController.getColorBlindMode.asInstanceOf[Var[UserSettingType]]
 }
 
 /**
@@ -73,3 +51,74 @@ val allUserSettings = List(
     NumSkipBreakpoints,
     ColorBlindMode
 )
+
+/**
+ * Updates all user settings to the provided new values.
+ *
+ * This function applies changes to the global settings by updating the respective reactive variables.
+ * The changes take effect immediately and can be observed in the user interface.
+ *
+ * @param newNumSkipIterativeChildren The new number of iterative children to skip.
+ * @param newNumSkipBreakpoints The new number of breakpoints to skip.
+ * @param newColorBlindMode The new value for enabling or disabling colour blind mode.
+ */
+def updateAllUserSettings(newNumSkipIterativeChildren: Int, newNumSkipBreakpoints: Int, newColorBlindMode: Boolean): Unit = {
+    NumSkipIterativeChildren.value.set(newNumSkipIterativeChildren)
+    NumSkipBreakpoints.value.set(newNumSkipBreakpoints)
+    ColorBlindMode.value.set(newColorBlindMode)
+
+}
+
+/**
+ * A user setting that determines the number of iterative children to be skipped.
+ *
+ * This setting controls how many iterative children are skipped when using the double arrow navigation.
+ * It is stored as an integer and can be modified by the user.
+ */
+case object NumSkipIterativeChildren extends UserSetting {
+    override def settingName: String = "Iterative children to skip"
+
+    override def infoText: String = "The number of iterative children to skip with double arrows"
+
+    override def style: String = "number"
+
+    override def default: UserSettingType = 5
+
+    override def value: Var[UserSettingType] = SettingsViewController.getNumSkipIterativeChildren.asInstanceOf[Var[UserSettingType]]
+}
+
+/**
+ * A user setting that determines the number of breakpoints to be skipped.
+ *
+ * When debugging, this setting defines how many breakpoints are skipped forwards when stepping through execution.
+ * It is stored as an integer and can be adjusted by the user.
+ */
+case object NumSkipBreakpoints extends UserSetting {
+    override def settingName: String = "Breakpoints to skip"
+
+    override def infoText: String = "The number of breakpoints to skip forwards when in debug mode"
+
+    override def style: String = "number"
+
+    override def default: UserSettingType = 1
+
+    override def value: Var[UserSettingType] = SettingsViewController.getNumSkipBreakpoints.asInstanceOf[Var[UserSettingType]]
+}
+
+/**
+ * A user setting that enables or disables colour blind mode.
+ *
+ * When activated, this setting adjusts the theme of the application to provide a more accessible viewing experience
+ * for users with colour vision deficiencies.
+ */
+case object ColorBlindMode extends UserSetting {
+    override def settingName: String = "Colour Blind Mode"
+
+    override def infoText: String = "Set Dill's theme to a more accessible viewing experience"
+
+    override def style: String = "boolean"
+
+    override def default: UserSettingType = false
+
+    override def value: Var[UserSettingType] = SettingsViewController.getColorBlindMode.asInstanceOf[Var[UserSettingType]]
+}
