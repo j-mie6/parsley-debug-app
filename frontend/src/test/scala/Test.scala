b9bc526ea5aae9b7083506e3ccb2588c5ca6f0bf
Adam <adamwatson1087@gmail.com>
2025-03-06 11:41:08 +0000

    fix: Frontend tests

--- frontend/src/test/scala/Test.scala
+++ frontend/src/test/scala/Test.scala
@@ -24,7 +24,7 @@ class Test extends AnyFlatSpec with should.Matchers {
             "isIterative": false,
             "newlyGenerated": false
         },
-        "isDebuggable": false
+        "sessionId": -1
     }"""
 
     
@@ -34,7 +34,7 @@ class Test extends AnyFlatSpec with should.Matchers {
 
         /* Check that the root tree has been deserialised correctly */
         tree.input should be ("Test")
-        tree.isDebuggable should be (false)
+        tree.sessionId should be (-1)
         tree.root.nodeId should be (0)
         tree.root.name should be ("Test")
         tree.root.internal should be ("Test")
