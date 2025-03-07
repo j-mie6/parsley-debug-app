903cff980ef90f6c01e703e7407d2811842d537b
aniket1101 <1.aniketgupta@gmail.com>
2025-03-07 02:01:53 +0000

    Feat: Added Input View styling

--- frontend/src/main/scala/controller/viewControllers/InputViewController.scala
+++ frontend/src/main/scala/controller/viewControllers/InputViewController.scala
@@ -3,30 +3,37 @@ package controller.viewControllers
 import com.raquo.laminar.api.L.*
 
 /**
- * Object containing functions and variables defining the input view,
- * which displays the input string of the tree from Parsley Debug
- */
+* Object containing functions and variables defining the input view,
+* which displays the input string of the tree from Parsley Debug
+*/
 object InputViewController {
-
+    
     /* The input element to be render by InputView */
     private val input: Var[Option[String]] = Var(None)
-
+    
     /** Set input string */
     val setInput: Observer[String] = input.someWriter
-
+    
     /** Set input to None to stop rendering */
     def unloadInput: Observer[Unit] = Observer(_ => input.set(None))
-
+    
     def getInputElem: Signal[HtmlElement] = input.signal.map(_ match 
-        /* Default tree view when no tree is loaded */
+    /* Default tree view when no tree is loaded */
         case None => 
-          div(
-            className := "tree-view-error",
-            "Nothing to show"
-          )
-
+            div(
+                className := "tree-view-error",
+                "Nothing to show"
+        )
+    
         /* Render as DebugTreeDisplay */
-        case Some(inputString) => div(className:= "debug-input-main-view", inputString)
+        case Some(inputString) => 
+            div(
+                className := "debug-input-container",
+                div(
+                    className := "debug-input-main-view",
+                    span(inputString) // Ensures correct text rendering
+                )
+        )
     )
-
+    
 }
