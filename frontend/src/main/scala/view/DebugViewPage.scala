c62f5fbebca5e83a294cbfd4a5b1ca1b32a58af6
aniket1101 <1.aniketgupta@gmail.com>
2025-03-07 16:45:51 +0000

    feat: Corrected button styling

--- frontend/src/main/scala/view/DebugViewPage.scala
+++ frontend/src/main/scala/view/DebugViewPage.scala
@@ -45,10 +45,10 @@ abstract class DebugViewPage extends Page {
 
     /* Export tree button */
     val downloadButton: HtmlElement = button(
-        className := "debug-view-file-button",
+        className := "debug-view-button debug-view-button-download",
 
         /* Save button icon */
-        i(className := "bi bi-floppy", fontSize.px := 22),
+        i(className := "bi bi-floppy"),
 
         /* Exports current tree */
         onClick(_
@@ -61,8 +61,8 @@ abstract class DebugViewPage extends Page {
 
     /* Uploading json element */
     val uploadButton: HtmlElement = label(
-        className := "debug-view-file-button",
-        i(className := "bi bi-cloud-download", fontSize.px := 25),
+        className := "debug-view-button debug-view-button-upload",
+        i(className := "bi bi-cloud-download"),
         input(
             typ := "file",
             display := "none",
